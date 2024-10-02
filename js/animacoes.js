f65e89a95044af4bc735d444304f9cac4af4485e
bl0nd <fernanda.v.loka@gmail.com>
2024-10-02 16:35:58 -0300

    ajustes

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,10 +1,28 @@
 // WOW JS
 new WOW().init();
 
+// BANNER SLICK
+// $('.banner').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+
 // GALERIA SLICK
 $('.galeria').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-  });
+});
+
+//FORM PARA WHATS
+document.getElementById('orcamento').addEventListener('submit', function (evento) {
+    evento.preventDefault(); // impede a ação padrão do form
+    EnviarWhats();
+});
+
+function EnviarWhats() {
+    alert("Teste de evento");
+};
