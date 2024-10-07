1be863388e4099ccfb66c02bdb6571d291fd5911
bl0nd <fernanda.v.loka@gmail.com>
2024-10-07 15:00:45 -0300

    pequenos ajustes

--- js/animacoes.js
+++ js/animacoes.js
@@ -1,13 +1,14 @@
 // WOW JS
+
 new WOW().init();
 
 // BANNER SLICK
-// $('.banner').slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2000,
-// });
+$('.banner').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
 
 // GALERIA SLICK
 $('.galeria').slick({
@@ -17,21 +18,15 @@ $('.galeria').slick({
     autoplaySpeed: 2000,
 });
 
-//FORM PARA WHATS
-document.getElementById('orcamento').addEventListener('submit', function (evento) {
-    evento.preventDefault(); // impede a ação padrão do form
-    EnviarWhats();
-});
-
 function EnviarWhats() {
-    // alert("Teste de evento");
-    // console.log("Cheguei aqui");
 
     var site = "*Site Ideia Tech 360 - Orçamento*";
-    var nome = document.getElementById('nomeOrcamento').value;
-    var fone = document.getElementById('foneOrcamento').value;
-    var email = document.getElementById('emailOrcamento').value;
-    var mens = document.getElementById('mensOrcamento').value;
+    var form = document.forms['orcamento'];
+
+    var nome = form.elements['nome'].value;
+    var fone = form.elements['fone'].value;
+    var email = form.elements['email'].value;
+    var mens = form.elements['mens'].value;
 
     var numWhats = '5511987521436';
     var quebraDeLinha = '%0A';
@@ -41,5 +36,7 @@ function EnviarWhats() {
     email = window.encodeURIComponent(email);
     mens = window.encodeURIComponent(mens);
 
-    window.open("https://wa.me/" + numWhats + "?text=" + site + quebraDeLinha + quebraDeLinha + nome + quebraDeLinha + fone + quebraDeLinha + email + quebraDeLinha + mens);
-};
+    window.open("https://wa.me/" + numWhats + "?text=" + site + quebraDeLinha + quebraDeLinha + nome + quebraDeLinha + fone + quebraDeLinha + email + quebraDeLinha + mens)
+    
+    window.open(url, '_blank');
+}
